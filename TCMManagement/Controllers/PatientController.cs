8421d64326934294f0437ad8f9cc596f40b32148
Shaohua <smao0875@conestogac.on.ca>
2018-02-03 19:17:19 -0500

    First commit

---
+++ TCMManagement/Controllers/PatientController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TCMManagement.Models;
+
+namespace TCMManagement.Controllers
+{
+    public class PatientController : ApiController
+    {
+        Patient[] patients = new Patient[]
+        {
+            new Patient { Id = 1, FirstName = "Bob", LastName = "Dylen", Address = "London", DateOfBirth = "2000-01-01", Email= "bob@gmail.com", Gender = "M", Phone = "1111111111" },
+            new Patient { Id = 2, FirstName = "Allen", LastName = "Dylen", Address = "London", DateOfBirth = "2000-01-01", Email= "bob@gmail.com", Gender = "M", Phone = "1111111111" },
+            new Patient { Id = 3, FirstName = "Rob", LastName = "Dylen", Address = "London", DateOfBirth = "2000-01-01", Email= "bob@gmail.com", Gender = "M", Phone = "1111111111" },
+            new Patient { Id = 4, FirstName = "Alice", LastName = "Dylen", Address = "London", DateOfBirth = "2000-01-01", Email= "bob@gmail.com", Gender = "M", Phone = "1111111111" },
+        };
+
+        public IEnumerable<Patient> GetAllPatients()
+        {
+            return patients;
+        }
+
+        public IHttpActionResult GetPatient(int id)
+        {
+            var patient = patients.FirstOrDefault((p) => p.Id == id);
+            if (patient == null)
+            {
+                return NotFound();
+            }
+            return Ok(patient);
+        }
+    }
+}
---
+++ TCMManagement/Models/Patient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TCMManagement.Models
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string DateOfBirth { get; set; }
+        public string Phone { get; set; }
+        public string Gender { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+}
