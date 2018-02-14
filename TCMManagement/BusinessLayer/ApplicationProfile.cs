bc192209a036e88a7ccfd306e1436d9d5a3fcc9c
smao0875 <smao0875@conestogac.on.ca>
2018-02-13 21:32:33 -0500

    Add initial code for swagger and AutoMapper

---
+++ TCMManagement/BusinessLayer/ApplicationProfile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using TCMManagement.Models;
+
+namespace TCMManagement.BusinessLayer
+{
+    public class ApplicationProfile : AutoMapper.Profile
+    {
+        public ApplicationProfile()
+        {
+            CreateMap<Person, Person>().ReverseMap();
+        }
+    }
+}
--- TCMManagement/Startup.cs
+++ TCMManagement/Startup.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Threading.Tasks;
 using System.Web.Http;
+using AutoMapper;
 using Microsoft.Owin;
 using Microsoft.Owin.Security.OAuth;
 using Owin;
+using TCMManagement.BusinessLayer;
+using TCMManagement.Models;
 
 [assembly: OwinStartup(typeof(TCMManagement.Startup))]
 
@@ -13,6 +16,7 @@ namespace TCMManagement
     {
         public void Configuration(IAppBuilder app)
         {
+            
             app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
 
             var myProvider = new TcmAuthorizationServerProvider();
@@ -24,10 +28,19 @@ namespace TCMManagement
                 Provider = myProvider
             };
 
+
             app.UseOAuthAuthorizationServer(options);
             app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
 
             HttpConfiguration config = new HttpConfiguration();
+
+            var mapperConfig = new AutoMapper.MapperConfiguration(c =>
+            {
+                c.AddProfile(new ApplicationProfile());
+            });
+            var mapper = mapperConfig.CreateMapper();
+            config.Services.Add(typeof(IMapper), mapper);
+
             WebApiConfig.Register(config);
         }
     }
