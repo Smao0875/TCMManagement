667001ef1591a0dce2bc3ded5513a5ab8941ec33
Jen <whitelilyhz@gmail.com>
2018-03-04 15:03:49 -0500

    The appointmentService unit test is working

--- TCMManagement.Test/TestAppointmentController/TestAppointmentService.cs
+++ TCMManagement.Test/TestAppointmentController/TestAppointmentService.cs
@@ -4,6 +4,11 @@ using System.Collections.Generic;
 using TCMManagement.BusinessLayer;
 using TCMManagement.Models;
 using Moq;
+using System.Data.Entity;
+using System.Linq;
+
+using System.Threading.Tasks;
+
 
 namespace TCMManagement.Test
 {
@@ -13,64 +18,85 @@ namespace TCMManagement.Test
         [TestMethod]
         public void TestCreateItem()
         {
-            Appointment[] appointmentList = {
-                new Appointment() { AppointmentId = 0, PersonId = 1, PatientId = 1, Description = "No Conflict", DateCreated = DateTime.Now,
-                                    TimeStart = DateTime.Parse("2018-03-02 09:00:00Z"), TimeEnd = DateTime.Parse("2018-03-02 10:00:00Z")},
-                new Appointment() { AppointmentId = 1, PersonId = 1, PatientId = 2, Description = "No Conflict", DateCreated = DateTime.Now,
-                                    TimeStart = DateTime.Parse("2018-03-02 10:00:00Z"), TimeEnd = DateTime.Parse("2018-03-02 11:00:00Z")},
-                new Appointment() { AppointmentId = 2, PersonId = 2, PatientId = 2, Description = "No Conflict", DateCreated = DateTime.Now,
-                                    TimeStart = DateTime.Parse("2018-03-02 15:00:00Z"), TimeEnd = DateTime.Parse("2018-03-02 16:00:00Z")},
+            // initialize the database with an appointment
+            var appointmentList = new List<Appointment>
+            {
+                new Appointment() { AppointmentId = 0, PersonId = 1, PatientId = 1, Description = "initilization 0", DateCreated = DateTime.Now,
+                                    TimeStart = DateTime.Parse("2018-03-02 09:00:00"), TimeEnd = DateTime.Parse("2018-03-02 10:00:00")},
+                new Appointment() { AppointmentId = 1, PersonId = 2, PatientId = 2, Description = "initilization 1", DateCreated = DateTime.Now,
+                                    TimeStart = DateTime.Parse("2018-03-02 15:00:00"), TimeEnd = DateTime.Parse("2018-03-02 16:00:00")}
+            };
+            var appointmentQuery = appointmentList.AsQueryable();
+            // the appointment list which has no conflict with the appointments in database
+            var appointmentNoConflictList = new List<Appointment>
+            {
+                new Appointment() { AppointmentId = 2, PersonId = 1, PatientId = 2, Description = "No Conflict 0", DateCreated = DateTime.Now,
+                                    TimeStart = DateTime.Parse("2018-03-02 10:00:00"), TimeEnd = DateTime.Parse("2018-03-02 11:00:00")}
+            };
+            // the appointment list which has conflict with the appointments in database
+            var appointmentConflictList = new List<Appointment>
+            {
                 new Appointment() { AppointmentId = 3, PersonId = 1, PatientId = 2, Description = "Conflict with 0 in start", DateCreated = DateTime.Now,
-                                    TimeStart = DateTime.Parse("2018-03-02 9:30:00Z"), TimeEnd = DateTime.Parse("2018-03-02 10:00:00Z")},
+                                    TimeStart = DateTime.Parse("2018-03-02 9:30:00"), TimeEnd = DateTime.Parse("2018-03-02 10:00:00")},
                 new Appointment() { AppointmentId = 4, PersonId = 2, PatientId = 1, Description = "Conflict with 2 in end", DateCreated = DateTime.Now,
-                                    TimeStart = DateTime.Parse("2018-03-02 14:00:00Z"), TimeEnd = DateTime.Parse("2018-03-02 15:30:00Z")},
+                                    TimeStart = DateTime.Parse("2018-03-02 14:00:00"), TimeEnd = DateTime.Parse("2018-03-02 15:30:00")},
             };
 
+            var mockSet = new Mock<DbSet<Appointment>>();
+            mockSet.As<IQueryable<Appointment>>().Setup(m => m.Provider).Returns(appointmentQuery.Provider);
+            mockSet.As<IQueryable<Appointment>>().Setup(m => m.Expression).Returns(appointmentQuery.Expression);
+            mockSet.As<IQueryable<Appointment>>().Setup(m => m.ElementType).Returns(appointmentQuery.ElementType);
+            mockSet.As<IQueryable<Appointment>>().Setup(m => m.GetEnumerator()).Returns(value : appointmentQuery.GetEnumerator());
 
-            var appointmentService = new AppointmentService();
-            var addedAppointment = appointmentService.CreateItem(appointmentList[0]);
-            Assert.AreEqual<int>(addedAppointment.AppointmentId, appointmentList[0].AppointmentId);
+            var mockContext = new Mock<TcmContext>();
+            mockContext.Setup(c => c.Appointments).Returns(mockSet.Object);
+            var appointmentService = new AppointmentService(mockContext.Object);
 
-            // no conflict even appointmentList[1].TimeStart == appointmentList[0].TimeEnd
-            addedAppointment = appointmentService.CreateItem(appointmentList[1]);
-            Assert.AreEqual<int>(addedAppointment.AppointmentId, appointmentList[1].AppointmentId);
-
-            appointmentService.CreateItem(appointmentList[2]);
+            // no conflict even appointmentNoConflictList[0].TimeStart == appointmentList[0].TimeEnd
+            var addedAppointment = appointmentService.CreateItem(appointmentNoConflictList[0]);
+            Assert.AreEqual<int>(addedAppointment.AppointmentId, 1);
+        
             // conflict with appointment 0 in TimeStart
-            addedAppointment = appointmentService.CreateItem(appointmentList[3]);
+            addedAppointment = appointmentService.CreateItem(appointmentConflictList[0]);
             Assert.AreEqual<Appointment>(null, null);
-
             // conflict with appointment 2 in TimeEnd
-            addedAppointment = appointmentService.CreateItem(appointmentList[4]);
+            addedAppointment = appointmentService.CreateItem(appointmentConflictList[1]);
             Assert.AreEqual<Appointment>(null, null);
         }
 
         [TestMethod]
         public void TestGetItemById()
         {
-            var appointmentService = new AppointmentService();
-            var appointment = new Appointment()
+            var appointmentList = new List<Appointment>
             {
-                AppointmentId = 0,
-                PersonId = 1,
-                PatientId = 1,
-                Description = "No Conflict",
-                DateCreated = DateTime.Now,
-                TimeStart = DateTime.Parse("2018-03-02 09:00:00Z"),
-                TimeEnd = DateTime.Parse("2018-03-02 10:00:00Z")
+                 new Appointment()
+                 {
+                    AppointmentId = 0,
+                    PersonId = 1,
+                    PatientId = 1,
+                    Description = "hurt",
+                    DateCreated = DateTime.Now,
+                    TimeStart = DateTime.Parse("2018-03-02 09:00:00"),
+                    TimeEnd = DateTime.Parse("2018-03-02 10:00:00")
+                 }
             };
+            var appointmentQuery = appointmentList.AsQueryable();
 
-            var addedAppointment = appointmentService.CreateItem(appointment);
-            var getAppointment = appointmentService.GetItemById(addedAppointment.AppointmentId);
-            Assert.AreEqual<int>(getAppointment.AppointmentId, addedAppointment.AppointmentId);
+            var mockSet = new Mock<DbSet<Appointment>>();
+            mockSet.As<IQueryable<Appointment>>().Setup(m => m.Provider).Returns(appointmentQuery.Provider);
+            mockSet.As<IQueryable<Appointment>>().Setup(m => m.Expression).Returns(appointmentQuery.Expression);
+            mockSet.As<IQueryable<Appointment>>().Setup(m => m.ElementType).Returns(appointmentQuery.ElementType);
+            mockSet.As<IQueryable<Appointment>>().Setup(m => m.GetEnumerator()).Returns(value: appointmentQuery.GetEnumerator());
+           
+            var mockContext = new Mock<TcmContext>();
+            mockContext.Setup(c => c.Appointments).Returns(mockSet.Object);
+            
+            var appointmentService = new AppointmentService(mockContext.Object);
+            var getAppointment = appointmentService.GetItemById(appointmentList[0].AppointmentId);
+            Assert.AreEqual<int>(getAppointment.AppointmentId, appointmentList[0].AppointmentId);
         }
 
         public void TestGetItems()
-        {
-
-        }
-
-        public void TestDeleteItem()
         {
 
         }
